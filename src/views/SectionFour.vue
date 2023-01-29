2508cd5055340d6b66dc53eb772b3b6a06daf231
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-01-29 18:46:18 +0300

    four section created in project

--- src/views/SectionFour.vue
+++ src/views/SectionFour.vue
@@ -1,3 +1,64 @@
 <script setup lang="ts">
+import { Post } from "../helper/interface";
+import NormPost from "../components/Posts/NormPost.vue";
+import startSection from "../components/icons/startSection.vue";
+import ArrowRight from "../components/icons/arrowRight.vue";
+import BaseBr from "../components/BaseBr.vue";
+import { ref } from "vue";
+const forNormPosts = ref<Post[]>([
+  {
+    url: "src/assets/images/fourSec1.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+  {
+    url: "src/assets/images/fourSec2.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+  {
+    url: "src/assets/images/fourSec3.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+  {
+    url: "src/assets/images/fourSec4.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+  {
+    url: "src/assets/images/fourSec5.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+  {
+    url: "src/assets/images/fourSec6.png",
+    text: "Оиладаги зўравонлик, аёллар ўлими, етим қолган болалар…",
+    type: "Ўзбекистон",
+    date: "2 кун олдин",
+  },
+]);
 </script>
-<template></template>
+<template>
+  <div class="w-full bg-white rounded-2xl p-5">
+    <div class="flex justify-between items-center">
+      <div class="flex items-center justify-start">
+        <startSection class="text-[#1D3068]" />
+        <p class="font-bold text-lg text-[#1D3068] ml-1">Мақолалар</p>
+      </div>
+      <div class="flex justify-end cursor-pointer items-center">
+        <p class="font-bold text-lg text-[#1D3068] mr-2">Барчаси</p>
+        <ArrowRight class="w-5 h-5" />
+      </div>
+    </div>
+    <BaseBr />
+    <div class="grid grid-cols-3 gap-4">
+        <NormPost v-for="(item, index) in forNormPosts" :post-data="item"/>
+    </div>
+  </div>
+</template>
